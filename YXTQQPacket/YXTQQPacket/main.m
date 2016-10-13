ca9d9b3a1f438e8f9e1dcce4d4f48d9f24a09da1
XT Y <yangxiaotong@yangxiaotongdemacbook-pro.local>
2016-10-13 23:13:19 +0800

    初始化项目

---
+++ YXTQQPacket/YXTQQPacket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YXTQQPacket
+//
+//  Created by 杨小童 on 16/10/13.
+//  Copyright © 2016年 YXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
